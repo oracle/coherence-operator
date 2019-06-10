adb8da78d14b0db187f4dc0782d20bc4ba00b071
Tim Middleton <tim.middleton@oracle.com>
2019-06-10 08:33:48 +0800

    initial envestigation

--- docs/samples/operator/rolling-upgrade/src/test/java/com/oracle/coherence/examples/testing/RollingUpgradeSampleIT.java
+++ docs/samples/operator/rolling-upgrade/src/test/java/com/oracle/coherence/examples/testing/RollingUpgradeSampleIT.java
@@ -145,7 +145,8 @@ public class RollingUpgradeSampleIT
             Eventually.assertThat(invoking(this).isValueUppercase(sCoherenceRelease),
                   is(true),
                   Timeout.after(600, TimeUnit.SECONDS),
-                  InitialDelay.of(10, TimeUnit.SECONDS));
+                  InitialDelay.of(10, TimeUnit.SECONDS),
+                  RetryFrequency.every(10, TimeUnit.SECONDS));
             }
         }
 
