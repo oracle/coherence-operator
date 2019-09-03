84d5237886ffb47c2dc7ec566df54bfa258dd274
Jonathan Knight <jk@thegridman.com>
2019-09-03 13:53:06 +0300

    Test code review check

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
 package apis
 
 import (
@@ -10,4 +16,13 @@ var AddToSchemes runtime.SchemeBuilder
 // AddToScheme adds all Resources to the Scheme
 func AddToScheme(s *runtime.Scheme) error {
 	return AddToSchemes.AddToScheme(s)
+}
+
+func Foo(i int) string {
+	// testing golint to detect bad code
+	if i == 0 {
+		return "i is zero"
+	} else {
+		return "i is not zero"
+	}
 }
