706f400351fd5dd8c7cea7ae282b04620c857445
Jonathan Knight <jk@thegridman.com>
2019-09-17 16:20:20 +0300

    Fixing release targets to release docs

--- hack/docs-unstable-index.sh
+++ hack/docs-unstable-index.sh
@@ -8,7 +8,7 @@ DIRS=$(ls -ld docs-unstable/* | grep '^d' | awk '{print $9}' | sort -r)
 echo "<html><body><h2>Unstable Release Documentation</h2><ul>" > docs-unstable/index.html;
 for i in ${DIRS}; do
   IFS='/' read -ra NAME <<< "${i}"
-  echo "<li><a href=https://oracle.github.io/coherence-operator/${i}index.html>${NAME[1]}</a></li>" >> docs-unstable/index.html
+  echo "<li><a href=https://oracle.github.io/coherence-operator/${i}/index.html>${NAME[1]}</a></li>" >> docs-unstable/index.html
 done
 echo "</ul></body></html>" >> docs-unstable/index.html
 
