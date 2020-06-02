1dc32c1665da85e499168a487d88b1663c101fcc
Jonathan Knight <jk@thegridman.com>
2020-06-02 20:42:50 +0300

    Remove checked-in operator-sdk

--- hack/ensure-sdk.sh
+++ hack/ensure-sdk.sh
@@ -7,9 +7,11 @@ UNAME_S=$(uname -s)
 UNAME_M=$(uname -m)
 OPERATOR_SDK=${CURRDIR}/etc/sdk/${UNAME_S}-${UNAME_M}/operator-sdk
 
+mkdir -p ${CURRDIR}/etc/sdk/${UNAME_S}-${UNAME_M} || true
+
 VERSION=$(${OPERATOR_SDK} version)
 echo "${VERSION}"
-echo ${VERSION} | grep 'operator-sdk version: "${REQUIRED_VERSION}"'
+echo ${VERSION} | grep "operator-sdk version: \"${REQUIRED_VERSION}\""
 if [[ $? == 1 ]]; then
   if [[ "${UNAME_S}" == "Linux" ]]; then
     curl -L https://github.com/operator-framework/operator-sdk/releases/download/${REQUIRED_VERSION}/operator-sdk-${REQUIRED_VERSION}-x86_64-linux-gnu -o ${OPERATOR_SDK}
@@ -17,5 +19,6 @@ if [[ $? == 1 ]]; then
   if [[ "${UNAME_S}" == "Darwin" ]]; then
     curl -L https://github.com/operator-framework/operator-sdk/releases/download/${REQUIRED_VERSION}/operator-sdk-${REQUIRED_VERSION}-x86_64-apple-darwin -o ${OPERATOR_SDK}
   fi
+  chmod +x ${OPERATOR_SDK}
 fi
 
