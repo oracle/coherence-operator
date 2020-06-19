b34be64912969a270f466a3a11c3fe25edb38310
Tim Middleton <tim.middleton@oracle.com>
2020-06-19 16:11:34 +0800

    fixup checkstyle

--- examples/deployment/src/main/java/com/oracle/coherence/examples/Person.java
+++ examples/deployment/src/main/java/com/oracle/coherence/examples/Person.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -10,7 +10,6 @@ import java.util.Objects;
 
 import com.tangosol.io.pof.annotation.Portable;
 import com.tangosol.io.pof.annotation.PortableProperty;
-
 
 /**
  * Sample server side class to represent a person.
