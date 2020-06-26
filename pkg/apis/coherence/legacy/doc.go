b7025a7d36cbf6990eda68b7dee35685bdf94e86
Jonathan Knight <jk@thegridman.com>
2020-06-26 11:45:17 +0300

    Add converter utility to convert v2 to v3 yaml

---
+++ pkg/apis/coherence/legacy/doc.go
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+// Package legacy contains API Schema definitions for the legacy Coherence Operator API
+// to be used by the converter utility to convert legacy yaml to the current yaml.
+//
+// +k8s:deepcopy-gen=package,register
+// +groupName=coherence.oracle.com
+package legacy
