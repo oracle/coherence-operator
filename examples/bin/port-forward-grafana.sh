a93535ae71adcc4f510dd2fd2e8b8d7ec3de87ff
Tim Middleton <tim.middleton@oracle.com>
2020-09-02 16:04:03 +0800

    Minor Updates to Deployment Example (#447)

--- examples/bin/port-forward-grafana.sh
+++ examples/bin/port-forward-grafana.sh
@@ -9,7 +9,7 @@ fi
 
 namespace=$1
 
-export GRAFANA_POD=$(kubectl get pods --namespace $namespace | grep grafana | awk '{print $1}')
+export GRAFANA_POD=$(kubectl get pods --namespace $namespace | grep prometheus-grafana | grep -v test | awk '{print $1}')
 echo "Port-forwarding $GRAFANA_POD in $namespace"
 
 echo "Open the following URL: http://127.0.0.1:3000/d/coh-main/coherence-dashboard-main"
