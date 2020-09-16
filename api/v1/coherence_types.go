664c3071f4be7734781275d5738983740ba8af40
Jonathan Knight <jk@thegridman.com>
2020-09-16 12:02:18 +0300

    Doc updates

--- api/v1/coherence_types.go
+++ api/v1/coherence_types.go
@@ -841,8 +841,11 @@ type NamedPortSpec struct {
 	// The port on each node on which this service is exposed when type=NodePort or LoadBalancer.
 	// Usually assigned by the system. If specified, it will be allocated to the service
 	// if unused or else creation of the service will fail.
+	// If set, this field must be in the range 30000 - 32767 inclusive.
 	// Default is to auto-allocate a port if the ServiceType of this Service requires one.
 	// More info: https://kubernetes.io/docs/concepts/services-networking/service/#type-nodeport
+	// +kubebuilder:validation:Minimum:=30000
+	// +kubebuilder:validation:Maximum:=32767
 	// +optional
 	NodePort *int32 `json:"nodePort,omitempty"`
 	// Number of port to expose on the host.
