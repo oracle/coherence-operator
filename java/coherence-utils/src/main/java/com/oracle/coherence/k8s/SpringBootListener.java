59b4b3680e103416f3f937ef69bd9e9eee40e0c1
Jonathan Knight <jk@thegridman.com>
2020-09-20 18:44:55 +0300

    Build fixes

--- java/coherence-utils/src/main/java/com/oracle/coherence/k8s/SpringBootListener.java
+++ java/coherence-utils/src/main/java/com/oracle/coherence/k8s/SpringBootListener.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
 package com.oracle.coherence.k8s;
 
 import org.apache.commons.logging.Log;
