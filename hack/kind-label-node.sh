f557e14386ad3ac5225cfe51c54e427a370fe5a7
Jonathan Knight <jk@thegridman.com>
2021-01-07 17:22:49 +0300

    Use a KinD cluster with a controller and two workers

--- hack/kind-label-node.sh
+++ hack/kind-label-node.sh
@@ -4,5 +4,3 @@ kubectl label node operator-worker topology.kubernetes.io/zone=zone-one --overwr
 kubectl label node operator-worker topology.kubernetes.io/region=one --overwrite
 kubectl label node operator-worker2 failure-domain.beta.kubernetes.io/zone=zone-two --overwrite
 kubectl label node operator-worker2 failure-domain.beta.kubernetes.io/region=two --overwrite
-kubectl label node operator-worker3 topology.kubernetes.io/zone=zone-three --overwrite
-kubectl label node operator-worker3 topology.kubernetes.io/region=three --overwrite
