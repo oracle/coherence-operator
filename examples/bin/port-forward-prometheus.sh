f1b504cfc8a8ec46d264a9804e1a4f6de778cb7f
Tim Middleton <tim.middleton@oracle.com>
2021-01-22 19:12:57 +0800

    Fix errors in Coherence Operator deployment example (#472)

--- examples/bin/port-forward-prometheus.sh
+++ examples/bin/port-forward-prometheus.sh
@@ -12,6 +12,8 @@ namespace=$1
 export PROM_POD=$(kubectl get pod --namespace $namespace | grep prometheus-0 | awk '{print $1}')
 echo "Port-forwarding $PROM_POD in $namespace"
 
+echo "Open the following URL: http://127.0.0.1:9090/targets"
+
 while :
 do
    kubectl port-forward --namespace $namespace  $PROM_POD 9090:9090
