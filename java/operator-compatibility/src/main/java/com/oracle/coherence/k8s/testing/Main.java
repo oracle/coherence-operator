5b6bb99d2527fb18a34510653259552e2895d812
Jonathan Knight <jk@thegridman.com>
2021-01-28 15:07:04 +0300

    Coherence compatibility tests

--- java/operator-compatibility/src/main/java/com/oracle/coherence/k8s/testing/Main.java
+++ java/operator-compatibility/src/main/java/com/oracle/coherence/k8s/testing/Main.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
 package com.oracle.coherence.k8s.testing;
 
 import com.tangosol.net.DefaultCacheServer;
