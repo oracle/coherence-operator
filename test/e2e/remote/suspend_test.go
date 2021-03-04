c6cda57f63c3cee80727ee434df7f7888bfa5ada
thegridman <jonathan.knight@oracle.com>
2021-03-05 00:40:14 +0300

    Harden StatusHA check to fail if distribution coordinator MBean is missing

--- test/e2e/remote/suspend_test.go
+++ test/e2e/remote/suspend_test.go
@@ -96,7 +96,7 @@ func TestNotSuspendServicesWhenSuspendDisabled(t *testing.T) {
 	// assert that the cache service is suspended
 	svc, err := ManagementOverRestRequest(&c, "/management/coherence/cluster/services/PartitionedCache")
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(svc["quorumStatus"]).To(ContainElement("Suspended"))
+	g.Expect(svc["quorumStatus"]).NotTo(ContainElement("Suspended"))
 
 	// remove the test finalizer which should then let everything be deleted
 	err = removeAllFinalizers(&c)
@@ -198,7 +198,7 @@ func TestNotSuspendServicesOnScaleDownToZeroIfSuspendDisabled(t *testing.T) {
 	// assert that the cache service is suspended
 	svc, err := ManagementOverRestRequest(&c, "/management/coherence/cluster/services/PartitionedCache")
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(svc["quorumStatus"]).To(ContainElement("Suspended"))
+	g.Expect(svc["quorumStatus"]).NotTo(ContainElement("Suspended"))
 
 	// remove the test finalizer from the StatefulSet and Coherence deployment which should then let everything be deleted
 	err = removeAllFinalizers(sts)
@@ -247,7 +247,7 @@ func TestNotSuspendServicesInMultipleDeployments(t *testing.T) {
 	// assert that the cache service is NOT suspended
 	svc, err := ManagementOverRestRequest(&cOne, "/management/coherence/cluster/services/PartitionedCache")
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(svc["quorumStatus"]).To(ContainElement("Suspended"))
+	g.Expect(svc["quorumStatus"]).NotTo(ContainElement("Suspended"))
 }
 
 func waitForFinalizerTasks(n types.NamespacedName) error {
