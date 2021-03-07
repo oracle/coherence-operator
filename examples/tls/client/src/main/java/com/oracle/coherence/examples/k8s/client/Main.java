1febabdb50d5b7ab14799255d35a7792179eb6a0
thegridman <jonathan.knight@oracle.com>
2021-03-07 16:46:06 +0300

    Update copyright headers

--- examples/tls/client/src/main/java/com/oracle/coherence/examples/k8s/client/Main.java
+++ examples/tls/client/src/main/java/com/oracle/coherence/examples/k8s/client/Main.java
@@ -1,8 +1,7 @@
 /*
- * Copyright (c) 2021., Oracle and/or its affiliates.
+ * Copyright (c) 2021, Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
- *
  */
 
 package com.oracle.coherence.examples.k8s.client;
