d4e7dc0e88e99b630e00dec04824be5291c47cce
thegridman <jonathan.knight@oracle.com>
2021-03-31 15:03:41 +0300

    Fix build to work on GitHub

---
+++ java/coherence-operator-client/src/main/java/com/oracle/coherence/k8s/client/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Classes to support running Coherence in Kubernetes.
+ */
+package com.oracle.coherence.k8s.client;
