19b76ae8474103eafc8c0d48d59362a9fc39af56
thegridman <jonathan.knight@oracle.com>
2021-09-07 18:31:48 +0300

    Fixing Makefile targets to build a debug enabled Operator image and allow remote debugging of a deployed operator

--- debug/debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-#
-# Copyright (c) 2021, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at
-# http://oss.oracle.com/licenses/upl.
-#
-
-dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient exec manager
