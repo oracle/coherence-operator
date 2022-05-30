c270b089fbe51d47d725a2f701829714d29898b1
Jonathan Knight <jk@thegridman.com>
2022-05-30 16:31:41 +0300

    Fixing backwards compatibility tests

--- test/e2e/remote/doc.go
+++ test/e2e/remote/doc.go
@@ -1,12 +1,9 @@
 /*
- * Copyright (c) 2019, 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2022, Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
 
-// The local package contains end-to-end Operator tests that require the
+// Package remote contains end-to-end Operator tests that require the
 // Operator to be deployed into the K8s cluster.
-// These tests use the operator-sdk end-to-end framework and must be run
-// using the "operator-sdk test local" command but without the "--up-local"
-// parameter.
 package remote
