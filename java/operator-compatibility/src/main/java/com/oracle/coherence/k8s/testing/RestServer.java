47f9b21530033d7abef8b7a9818c418e678f65b7
Jonathan Knight <jk@thegridman.com>
2022-09-03 15:43:52 +0300

    Fix Coherence compatibility tests to run correct main class

--- java/operator-compatibility/src/main/java/com/oracle/coherence/k8s/testing/RestServer.java
+++ java/operator-compatibility/src/main/java/com/oracle/coherence/k8s/testing/RestServer.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.Cluster;
+import com.tangosol.net.Coherence;
 import com.tangosol.net.DefaultCacheServer;
 import com.tangosol.net.DistributedCacheService;
 import com.tangosol.net.NamedCache;
@@ -67,10 +68,19 @@ public class RestServer {
             thrown.printStackTrace();
         }
 
-        String main = System.getProperty("coherence.k8s.testing.main", DefaultCacheServer.class.getName());
+        String main = System.getProperty("coherence.k8s.testing.main", getMainClass());
         Class<?> cls = Class.forName(main);
         Method method = cls.getMethod("main", String[].class);
         method.invoke(method, (Object) args);
+    }
+
+    private static String getMainClass() {
+        try {
+            return Coherence.class.getCanonicalName();
+        }
+        catch (Throwable e) {
+            return DefaultCacheServer.class.getCanonicalName();
+        }
     }
 
     private static void send(HttpExchange t, int status, String body) throws IOException {
