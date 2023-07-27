c1fbe81bd58bf778c43c0d4746c8f246507ebd60
Jonathan Knight <jonathan.knight@oracle.com>
2023-07-27 17:29:30 +0300

    Fix version check to work with interim Coherence releases (#609)

--- java/coherence-operator/src/test/java/com/oracle/coherence/k8s/CoherenceVersionTest.java
+++ java/coherence-operator/src/test/java/com/oracle/coherence/k8s/CoherenceVersionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -50,4 +50,11 @@ public class CoherenceVersionTest {
         assertThat(CoherenceVersion.versionCheck("1.1.1.1.1", "1.1.1.1.2"), is(false));
         assertThat(CoherenceVersion.versionCheck("1.2", "2.1"), is(false));
     }
+
+    @Test
+    public void shouldWorkWithInterimBuild() throws Exception {
+        assertThat(CoherenceVersion.versionCheck("14.1.1.0.15 (101966-Int)", "14.1.1.0.0"), is(true));
+        assertThat(CoherenceVersion.versionCheck("14.1.1.0.15 (101966-Int)", "22.06.0"), is(false));
+    }
+
 }
