4ea457b20c2b0971b2e5e21f74a362664da05ea7
Github Action <thegridman@users.noreply.github.com>
2024-04-05 16:30:09 +0000

    Release Coherence Operator version: 3.3.4

---
+++ docs/3.3.4/pages/examples/900_demo/README.js
@@ -0,0 +1,20 @@
+<doc-view>
+<p>The Coherence Demonstration application is an application which demonstrates various Coherence
+related features such include Persistence, Federation and Lambda support.  This demonstration
+can run stand alone but can also be installed on the Coherence Operator.</p>
+
+<p>When installed using the Coherence Operator, the setup includes two Coherence Clusters, in the same Kubernetes cluster,
+which are configured with Active/Active Federation.</p>
+
+
+
+<v-card>
+<v-card-text class="overflow-y-hidden" style="text-align:center">
+<img src="./images/images/coherence-demo.png" alt="Service Details"width="950" />
+</v-card-text>
+</v-card>
+
+<p><img src="./images/GitHub-Mark-32px.png" alt="GitHub Mark 32px" />
+ Please see <a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-demo">The Coherence Demo GitHub project</a> for full instructions.</p>
+
+</doc-view>
