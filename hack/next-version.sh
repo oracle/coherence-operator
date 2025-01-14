906797b3c5d871b456dacb612587a0b74ab334d3
Jonathan Knight <jk@thegridman.com>
2025-01-14 17:12:54 +0300

    Bump version to 3.4.2

---
+++ hack/next-version.sh
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2020, 2025, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# http://oss.oracle.com/licenses/upl.
+#
+
+echo $1 | awk -F. '{OFS="."; $NF+=1; print $0}'
