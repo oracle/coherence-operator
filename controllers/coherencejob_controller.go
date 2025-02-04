7692cc57988d7fbecb64890275d7bedfa78540bf
Jonathan Knight <jk@thegridman.com>
2025-02-04 11:51:59 +0300

    Fix hash code reconciliation

--- controllers/coherencejob_controller.go
+++ controllers/coherencejob_controller.go
@@ -160,38 +160,12 @@ func (in *CoherenceJobReconciler) ReconcileDeployment(ctx context.Context, reque
 	}
 
 	hash := deployment.GetLabels()[coh.LabelCoherenceHash]
+	storeHash, _ := storage.GetHash()
 	var desiredResources coh.Resources
 
-	storeHash, found := storage.GetHash()
-	if !found || storeHash != hash || deployment.Status.Phase != coh.ConditionTypeReady {
-		// Storage state was saved with no hash or a different hash so is not in the desired state
-		// or the Coherence resource is not in the Ready state
-		// Create the desired resources the deployment
-		if desiredResources, err = deployment.CreateKubernetesResources(); err != nil {
-			return in.HandleErrAndRequeue(ctx, err, nil, fmt.Sprintf(createResourcesFailedMessage, request.Name, request.Namespace, err), in.Log)
-		}
-
-		if found {
-			// The "storeHash" is not "", so it must have been processed by the Operator (could have been a previous version).
-			// There was a bug prior to 3.4.2 where the hash was calculated at the wrong point in the defaulting web-hook,
-			// and the has used only a portion of the spec, so the "currentHash" may be wrong, and hence differ from the
-			// recalculated "hash".
-			if deployment.IsBeforeVersion("3.4.2") {
-				// the AnnotationOperatorVersion annotation was added in the 3.2.8 web-hook, so if it is missing
-				// the Coherence resource was added or updated prior to 3.2.8, or the version is present but is
-				// prior to 3.4.2. In this case we just ignore the difference in hash.
-				// There is an edge case where the Coherence resource could have legitimately been updated whilst
-				// the Operator and web-hooks were uninstalled. In that case we would ignore the update until another
-				// update is made. The simplest way for the customer to work around this is to add the
-				// AnnotationOperatorVersion annotation with some value, which will then be overwritten by the web-hook
-				// and the Coherence resource will be correctly processes.
-				desiredResources = storage.GetLatest()
-				log.Info("Ignoring hash difference for pre-3.4.2 resource", "hash", hash, "store", storeHash)
-			}
-		}
-	} else {
-		// storage state was saved with the current hash so is already in the desired state
-		desiredResources = storage.GetLatest()
+	desiredResources, err = checkJobHash(deployment, storage, log)
+	if err != nil {
+		return in.HandleErrAndRequeue(ctx, err, nil, fmt.Sprintf(createResourcesFailedMessage, request.Name, request.Namespace, err), in.Log)
 	}
 
 	// create the result
