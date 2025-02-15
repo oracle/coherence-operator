9f0d62a7c70c9f74874c9a261ab369d48ed606cd
Github Action <thegridman@users.noreply.github.com>
2025-02-15 12:21:27 +0000

    Release Coherence Operator version: 3.4.2

---
+++ docs/3.4.2/pages/docs/networking/010_overview.js
@@ -0,0 +1,29 @@
+<doc-view>
+
+<h2 id="_overview">Overview</h2>
+<div class="section">
+<p>This section describes how Coherence and the Operator work with different Kubernetes networking configurations.</p>
+
+<v-layout row wrap class="mb-5">
+<v-flex xs12>
+<v-container fluid grid-list-md class="pa-0">
+<v-layout row wrap class="pillars">
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/docs/networking/020_dual_stack"><div class="card__link-hover"/>
+</router-link>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Dual Stack Clusters</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Running on a dual-stack Kubernetes cluster</p>
+</v-card-text>
+</v-card>
+</v-flex>
+</v-layout>
+</v-container>
+</v-flex>
+</v-layout>
+</div>
+</doc-view>
