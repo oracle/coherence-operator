7108dbbb5fdc8d35c5c32618b2acfd8da101cfb9
Jonathan Knight <jk@thegridman.com>
2025-02-25 19:40:54 +0300

    Spring console fixes

--- runner/main.go
+++ runner/main.go
@@ -24,6 +24,8 @@ var (
 	Version string
 	// Commit is the runner Git commit injected by the Go linker at build time.
 	Commit string
+	// Branch is the runner Git branch injected by the Go linker at build time.
+	Branch string
 	// Date is the runner build date injected by the Go linker at build time.
 	Date string
 	// Author is the username of the account at build time
@@ -46,7 +48,7 @@ func printVersion() {
 	log.Info(fmt.Sprintf("Operator Version: %s", Version))
 	log.Info(fmt.Sprintf("Operator Build Date: %s", Date))
 	log.Info(fmt.Sprintf("Operator Built By: %s", Author))
-	log.Info(fmt.Sprintf("Operator Git Commit: %s", Commit))
+	log.Info(fmt.Sprintf("Operator Git Commit: %s (%s)", Commit, Branch))
 	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
 	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
 }
