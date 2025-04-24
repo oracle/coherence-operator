6c450d051423c25533fe5b01f36d58e2f9e1223b
Jonathan Knight <jk5766@outlook.com>
2025-04-24 15:49:49 +0300

    Fix OpenShift CLI download script

--- hack/openshift/get-oc.sh
+++ hack/openshift/get-oc.sh
@@ -12,29 +12,25 @@ TOOLS_BIN=${ROOT_DIR}/build/tools/bin
 UNAME_S=$(uname -s)
 UNAME_M=$(uname -m)
 
-rm -rf oc-tmp
-mkdir oc-tmp
-
 if [ "Darwin" = "${UNAME_S}" ]; then
   if [ "x86_64" = "${UNAME_M}" ]; then
     echo "Downloading OpenShift OC CLI ${UNAME_S} ${UNAME_M}"
-  	curl -Ls https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-mac.tar.gz -o oc-tmp/openshift-client.tar.gz
+  	curl -Ls https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-mac.tar.gz -o openshift-client.tar.gz
   else
     echo "Downloading OpenShift OC CLI ${UNAME_S} ${UNAME_M}"
-    curl -Ls https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/stable/openshift-client-mac-arm64.tar.gz -o oc-tmp/openshift-client.tar.gz
+    curl -Ls https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/stable/openshift-client-mac-arm64.tar.gz -o openshift-client.tar.gz
   fi
 else
   if [ "x86_64" = "${UNAME_M}" ]; then
     echo "Downloading OpenShift OC CLI ${UNAME_S} ${UNAME_M}"
-    curl -Ls https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz -o oc-tmp/openshift-client.tar.gz
+    curl -Ls https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz -o openshift-client.tar.gz
   else
     echo "Downloading OpenShift OC CLI ${UNAME_S} ${UNAME_M}"
-    curl -Ls https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/stable/openshift-client-linux.tar.gz -o oc-tmp/openshift-client.tar.gz
+    curl -Ls https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/stable/openshift-client-linux.tar.gz -o openshift-client.tar.gz
   fi
 fi
 
-cd oc-tmp && tar -xvf openshift-client.tar.gz
-mv oc-tmp/oc ${TOOLS_BIN}/oc
-rm -rf oc-tmp
-
+tar -zxvf openshift-client.tar.gz oc
+mv oc ${TOOLS_BIN}/oc
 chmod +x ${TOOLS_BIN}/oc
+rm openshift-client.tar.gz
