c5791ec06dbb7226e6a33cf11f74bcfc326a0c24
Jonathan Knight <jk5766@outlook.com>
2025-06-19 13:42:13 +0300

    Fix broken tests

--- java/operator-compatibility/src/main/java/com/oracle/coherence/k8s/testing/Setup.java
+++ java/operator-compatibility/src/main/java/com/oracle/coherence/k8s/testing/Setup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2025, Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -44,6 +44,8 @@ public class Setup {
         if (source.toFile().exists()) {
             Path target = targetDir.resolve(source.getFileName());
             Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+        } else {
+            throw new IllegalStateException("Source does not exist: " + source);
         }
     }
 }
