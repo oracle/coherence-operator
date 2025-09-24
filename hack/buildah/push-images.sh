541f87556d0f86ffd969f5454ef1a80acb9da8f4
Jonathan Knight <jonathan.knight@oracle.com>
2025-09-24 20:48:59 +0300

    Adding example operand image for Red Hat certification (#809)

--- hack/buildah/push-images.sh
+++ hack/buildah/push-images.sh
@@ -8,37 +8,51 @@ set -x -e -v
 
 buildah version
 
-if [ "${OPERATOR_IMAGE_REGISTRY}" != "" ] && [ "${REGISTRY_USERNAME}" != "" ] && [ "${REGISTRY_PASSWORD}" != "" ]
+if [ "${IMAGE_NAME_REGISTRY}" != "" ] && [ "${REGISTRY_USERNAME}" != "" ] && [ "${REGISTRY_PASSWORD}" != "" ]
 then
-  buildah login -u "${REGISTRY_USERNAME}" -p "${REGISTRY_PASSWORD}" "${OPERATOR_IMAGE_REGISTRY}"
+  buildah login -u "${REGISTRY_USERNAME}" -p "${REGISTRY_PASSWORD}" "${IMAGE_NAME_REGISTRY}"
+fi
+
+if [ "${IMAGE_NAME_AMD}" == "" ]
+then
+  IMAGE_NAME_AMD="${IMAGE_NAME}-amd64"
+fi
+
+if [ "${IMAGE_NAME_ARM}" == "" ]
+then
+  IMAGE_NAME_ARM="${IMAGE_NAME}-arm64"
 fi
 
 if [ "${NO_DOCKER_DAEMON}" != "true" ]
 then
-  buildah pull "docker-daemon:${OPERATOR_IMAGE_AMD}"
-  buildah pull "docker-daemon:${OPERATOR_IMAGE_ARM}"
+  buildah pull "docker-daemon:${IMAGE_NAME_AMD}"
+  buildah pull "docker-daemon:${IMAGE_NAME_ARM}"
 fi
 
-buildah rmi "${OPERATOR_IMAGE}" || true
-buildah manifest rm "${OPERATOR_IMAGE}" || true
+buildah rmi "${IMAGE_NAME}" || true
+buildah manifest rm "${IMAGE_NAME}" || true
+
 buildah manifest create \
     --annotation org.opencontainers.image.source=https://github.com/oracle/coherence-operator \
     --annotation org.opencontainers.image.licenses="UPL-1.0" \
     --annotation org.opencontainers.image.version="${VERSION}" \
     --annotation org.opencontainers.image.revision="${REVISION}" \
-    "${OPERATOR_IMAGE}"
+    "${IMAGE_NAME}"
+
 buildah manifest add --arch amd64 --os linux \
     --annotation org.opencontainers.image.source=https://github.com/oracle/coherence-operator \
     --annotation org.opencontainers.image.licenses="UPL-1.0" \
     --annotation org.opencontainers.image.version="${VERSION}" \
     --annotation org.opencontainers.image.revision="${REVISION}" \
-    "${OPERATOR_IMAGE}" "${OPERATOR_IMAGE_AMD}"
+    "${IMAGE_NAME}" "${IMAGE_NAME_AMD}"
+
 buildah manifest add --arch arm64 --os linux \
     --annotation org.opencontainers.image.source=https://github.com/oracle/coherence-operator \
     --annotation "org.opencontainers.image.licenses"="UPL-1.0" \
     --annotation org.opencontainers.image.version="${VERSION}" \
     --annotation org.opencontainers.image.revision="${REVISION}" \
-    "${OPERATOR_IMAGE}" "${OPERATOR_IMAGE_ARM}"
-buildah manifest inspect "${OPERATOR_IMAGE}"
+    "${IMAGE_NAME}" "${IMAGE_NAME_ARM}"
 
-buildah manifest push --all -f v2s2 "${OPERATOR_IMAGE}" "docker://${OPERATOR_IMAGE}"
+buildah manifest inspect "${IMAGE_NAME}"
+
+buildah manifest push --all -f v2s2 "${IMAGE_NAME}" "docker://${IMAGE_NAME}"
